db1866ce428e0793dc83e3e45b5b06ce7de16669
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2023-04-07 00:14:25 +0630

    Update autofilter.py

--- bot/plugins/autofilter.py
+++ bot/plugins/autofilter.py
@@ -15,8 +15,20 @@ from ..utils.logger import LOGGER
 log = LOGGER(__name__)
 
 
-@Bot.on_message(filters.group & filters.text & filters.incoming, group=-1)  # type: ignore
-async def give_filter(bot: Bot, message: types.Message):
+@Bot.on_message(filters.group & filters.text & filters.incoming, group=-1)
+async def auto_filter(bot: Bot, message: types.Message, text=True):
+    #if not await check_fsub(bot, message):
+        #return 
+    settings = await config_db.get_settings(f"SETTINGS_{message.chat.id}")
+    if settings['CH_POST']:
+        kt = await ch9_imdb(bot, message)
+        return
+        a = await ch1_give_filter(bot, message)
+
+
+
+
+async def ch1_give_filter(bot: Bot, message: types.Message):
 
     if message.text.startswith("/"):
         return  # ignore commands
@@ -67,7 +79,7 @@ async def give_filter(bot: Bot, message: types.Message):
         btn = [
             [
                 types.InlineKeyboardButton(
-                    "Download", url=f"https://t.me/{bot.me.username}?start=filter{key}"
+                    f"📥  {search}  📥", url=f"https://t.me/{bot.me.username}?start=filter{key}"
                 )
             ]
         ]
@@ -79,7 +91,8 @@ async def give_filter(bot: Bot, message: types.Message):
             **locals(),
         )
     else:
-        cap = f"Query: {search}\nTotal Results: {total_results}"
+        cap = f"𝗤𝘂𝗲𝗿𝘆 : {search}\n𝗧𝗼𝘁𝗮𝗹  : {total_results}\n𝗥𝗲𝗾𝘂𝗲𝘀𝘁 : {message.from_user.mention} \n\n</b><a href='https://t.me/+6lHs-byrjxczY2U1'>©️ 𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟</a>"
+	
     if imdb and imdb.get("poster") and settings["IMDB_POSTER"]:  # type: ignore
         try:
             await message.reply_photo(
@@ -216,15 +229,157 @@ async def handle_file(bot: Bot, query: types.CallbackQuery):
         return await query.answer("Please Join My Update Channel and click again")
     try:
         await bot.send_cached_media(
-            query.from_user.id,
-            file_id,  # type: ignore
-            caption=Config.CUSTOM_FILE_CAPTION.format(  # type: ignore
+                Config.FILE_CHANNEL,
+                file_id,
+                caption=Config.CUSTOM_FILE_CAPTION.format(  # type: ignore
+                file_name=file_info["file_name"],
+                file_size=get_size(file_info["file_size"]),
+                caption=file_info["caption"],
+            ),                
+                reply_to_message_id=query.message.id,
+        )
+         
+        #await bot.send_message(chat_id=query.from_user.id, text=f"👋 Hello {query.from_user.mention},Happy Downloading and Come Again... \n\nပျော်ရွှင်စွာဒေါင်းလုဒ်လုပ်ပြီး ‌နောက်ထပ်လာခဲ့ပါ... \n\nကျွန်တော်တို့ ချန်နယ်လ်မှ ဇာတ်ကားများကြည့်ရှုသည့်အတွက် ကျေးဇူးအများကြီးတင်ပါတယ် ခင်ဗျာ။🙏🙏🙏\n\n👉🏻 @MKS_REQUESTGROUP ❤️❤️")        
+        file_send = await bot.send_cached_media(
+                chat_id=Config.FILE_CHANNEL2,
+                file_id=file_id,
+                caption=Config.CUSTOM_FILE_CAPTION2.format(
                 file_name=file_info["file_name"],
                 file_size=get_size(file_info["file_size"]),
                 caption=file_info["caption"],
             ),
-            reply_to_message_id=query.message.id,
+                reply_markup=types.InlineKeyboardMarkup(
+                    [
+                        [types.InlineKeyboardButton("Join Channel", url="https://t.me/+6lHs-byrjxczY2U1")],
+                        [types.InlineKeyboardButton("Group Link ", url="https://t.me/+X7DNvf9iCy5jOGJl")]
+                    ]
+            ),
+                reply_to_message_id=query.message.id,
         )
+        caption1 = f"⚠️{query.from_user.mention} \n\nအချောလေး ရှာတဲ့ဇာတ်ကား အဆင့်သင့်ပါ ⬇️ "
+        settings = await config_db.get_settings(f"SETTINGS_{query.message.chat.id}")
+        if not settings["DOWNLOAD_BUTTON"]:
+            await query.message.reply_text(                
+                caption1,
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton('Join Channel Link', url="https://t.me/+H7ERsk_04EoxOTU1")],
+                        [InlineKeyboardButton(f'📥 {file_info["file_name"]} {file_info["caption"]}📥', url=file_send.link)]
+                    ]
+                ),
+                quote=True,
+                disable_web_page_preview=True,
+            )
+        else:
+            await bot.send_message(
+                chat_id=query.from_user.id,                
+                text=caption1,
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [InlineKeyboardButton( 'Join Channel link', url="https://t.me/+H7ERsk_04EoxOTU1")],
+                        [InlineKeyboardButton(f'📥 {file_info["file_name"]} {file_info["caption"]} 📥', url=file_send.link)]
+                    ]
+                )
+            )
+        await asyncio.sleep(86400)
+        await file_send.delete()
     except errors.PeerIdInvalid:
         return await query.answer(f"https://t.me/{bot.me.username}?start=okok")
-    await query.answer(f'Sending : {file_info["file_name"]}')
+    await query.answer(f'Sending : သင်နှိပ်လိုက်တဲ့ ဇာတ်ကားအား Bot Direct Message သို့ပေးပို့လိုက်ပါပြီ \n\nCheck bot Direct Message \n\n {file_info["file_name"]}')	
+
+
+
+async def ch9_imdb(bot: Bot, message: types.Message, text=True):
+    if message.text.startswith("/"):
+        return  # ignore commands
+
+    if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F()\d]).*)", message.text): 
+        return
+
+    if 2 < len(message.text) < 150:
+        settings = await config_db.get_settings(f"SETTINGS_{message.chat.id}")
+        search = message.text
+        files, offset, total_results = await a_filter.get_search_results(
+            search.lower(), offset=0, filter=True
+        )
+        if not files:
+            return
+    else:
+        return
+    key = f"{message.chat.id}-{message.id}"
+
+    Cache.BUTTONS[key] = search
+    
+    if settings["IMDB"]:
+        imdb = await get_poster(search, file=(files[0])["file_name"])
+	
+    else:
+        imdb = {}
+
+    cap = f"{search}\n\n"
+    
+    if imdb:
+        
+        cap += Config.TEMPLATE8.format(
+            query=search,
+            **imdb,
+            **locals(),
+        )
+
+    buttons = []
+    #for i, file in enumerate(files):
+        #cap += f"[ဇာတ်ကားကြည့်ရန် ဤနေရာကိုနှိပ်ပါ Link {i+1}]({await parse_link(file['chat_id'], file['message_id'])})\n\n"
+        
+    #Cache.SEARCH_DATA[key] = files, offset, total_results, imdb, settings
+    cap += f"""<a href='https://t.me/+X7DNvf9iCy5jOGJl'>ဇာတ်ကား ကြည့်ရန်
+အောက်က Link ကို Join ပါ</a>
+https://t.me/+X7DNvf9iCy5jOGJl
+
+ဂရုထဲရောက်ရင်
+👉<code>  {search}  </code> 👈 ဟုရိုက်ပြီး
+ ကြည့်ရှုနိုင်ပါသည်။"""
+ 
+    if imdb and imdb.get("poster") and settings["IMDB_POSTER"]:
+        try:
+            await message.reply_photo(
+                photo=imdb.get("poster"),
+                caption=cap,
+                quote=True,
+            )
+            await bot.send_photo(
+                Config.FILE_CHANNEL8,
+		photo=imdb.get("poster"),
+                caption=cap,
+	    )
+
+        except (
+            errors.MediaEmpty,
+            errors.PhotoInvalidDimensions,
+            errors.WebpageMediaEmpty,
+        ):
+            pic = imdb.get("poster")
+            poster = pic.replace(".jpg", "._V1_UX360.jpg")
+            await message.reply_photo(
+                photo=poster,
+                caption=cap[:1024],                
+                quote=True,
+	    )
+            await bot.send_photo(
+                Config.FILE_CHANNEL8,
+                photo=poster,
+                caption=cap[:1024], 
+	    )
+        except Exception as e:
+            log.exception(e)
+            await message.reply_text(
+                cap, quote=True
+            )
+    else:
+        await message.reply_text(
+            cap,            
+            quote=True,
+            disable_web_page_preview=True,
+        )
+
+ 
+	
