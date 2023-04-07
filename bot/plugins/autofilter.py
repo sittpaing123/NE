582a22f99c61ce4a152ce8b664d586dd861cd98a
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2023-04-07 11:41:43 +0630

    Update autofilter.py

--- bot/plugins/autofilter.py
+++ bot/plugins/autofilter.py
@@ -319,7 +319,9 @@ https://t.me/+X7DNvf9iCy5jOGJl
 ဂရုထဲရောက်ရင်
 👉<code>  {search}  </code> 👈 ဟုရိုက်ပြီး
 
-ဇာတ်ကားကို ကြည့်ရှုနိုင်ပါသည်။"""
+ဇာတ်ကားကို ကြည့်ရှုနိုင်ပါသည်။
+
+<a href='https://t.me/+6lHs-byrjxczY2U1'>©️ 𝗙𝗜𝗟𝗘 𝗦𝗧𝗢𝗥𝗘 𝗖𝗛𝗔𝗡𝗡𝗘𝗟</a><a href='https://t.me/+X7DNvf9iCy5jOGJl'>©️ 𝗝𝗢𝗜𝗡 𝗚𝗥𝗢𝗨𝗣</a><a href='https://t.me/+6lHs-byrjxczY2U1'>©️ 𝗝𝗢𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟</a>"""
  
     if imdb and imdb.get("poster") and settings["IMDB_POSTER"]:
         try:
