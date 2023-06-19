00e710e205ff7186f54cbfede77e3d9347ed669d
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2023-06-19 23:52:52 +0630

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -330,7 +330,7 @@ async def get_stats(_, msg: types.Message):
     users = await usersDB.total_users_count()
     free = 536870912 - size
 
-    currentTime = time.strftime("% Hh % Mm % Ss", time.gmtime(time.time() - BOT_START_TIME))
+    currentTime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - BOT_START_TIME))
     total, used, free2 = shutil.disk_usage(".")
     total = humanbytes(total)
     used = humanbytes(used)
@@ -340,10 +340,10 @@ async def get_stats(_, msg: types.Message):
     disk_usage = psutil.disk_usage('/').percent
     buttons = [[types.InlineKeyboardButton('𝚁𝙴𝙵𝚁𝙴𝚂𝙷 ♻️', callback_data='rfrsh')]]
     await msg.reply(
-        f"**Stats**\n\n**Total Files**: `{count}`"
+        f"**Stats**\n\nTotal Files: `{count}`"
         f"\nTotal Users: {users}"
-        f"\n**Total DB Used:** `{get_size(size)}`"
-        f"\n**Free:** `{get_size(free)}`"
+        f"\nTotal DB Used: `{get_size(size)}`"
+        f"\nFree: `{get_size(free)}`"
         f"\n\nUptime: {currentTime}"
         f"\nCPU Usage: {cpu_usage}%"
         f"\nRAM Usage: {ram_usage}%"        
@@ -362,7 +362,7 @@ async def ref_get_stats(bot: Bot, query: types.CallbackQuery):
     users = await usersDB.total_users_count()
     free = 536870912 - size
 
-    currentTime = time.strftime("% Hh % Mm % Ss", time.gmtime(time.time() - BOT_START_TIME))
+    currentTime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - BOT_START_TIME))
     total, used, free2 = shutil.disk_usage(".")
     total = humanbytes(total)
     used = humanbytes(used)
@@ -371,14 +371,14 @@ async def ref_get_stats(bot: Bot, query: types.CallbackQuery):
     ram_usage = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage('/').percent
     buttons = [[types.InlineKeyboardButton('𝚁𝙴𝙵𝚁𝙴𝚂𝙷 ♻️', callback_data='rfrsh')]]
-    await msg.reply(
-        f"**Stats**\n\n**Total Files**: `{count}`"
+    text = (
+        f"**Stats**\n\nTotal Files: `{count}`"
         f"\nTotal Users: {users}"
-        f"\n**Total DB Used:** `{get_size(size)}`"
-        f"\n**Free:** `{get_size(free)}`"
+        f"\nTotal DB Used: `{get_size(size)}`"
+        f"\nFree: `{get_size(free)}`"
         f"\n\nUptime: {currentTime}"
         f"\nCPU Usage: {cpu_usage}%"
-        f"\nRAM Usage: {ram_usage}%"        
+        f"\nRAM Usage: {ram_usage} %"        
         f"\nTotal Disk Space: {total}"
         f"\nUsed Space: {used} ({disk_usage}%)"
         f"\nFree Space: {free2}"
@@ -386,6 +386,11 @@ async def ref_get_stats(bot: Bot, query: types.CallbackQuery):
         reply_markup=types.InlineKeyboardMarkup(buttons),
         parse_mode=enums.ParseMode.HTML    
     )
+    await query.message.edit_text(
+        text,
+        reply_markup=types.InlineKeyboardMarkup(buttons),
+        parse_mode=enums.ParseMode.HTML
+    ) 
 
 @Bot.on_message(filters.command("delete") & filters.user(Config.ADMINS))  # type: ignore
 async def handleDelete(bot: Bot, msg: types.Message):
