f476a74c1e568586b92a56b1a65a93418e6d35f1
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2023-07-06 00:29:40 +0630

    Update clone.py

--- bot/plugins/clone.py
+++ bot/plugins/clone.py
@@ -33,7 +33,7 @@ async def on_clone(self, message):
                 ai = Client(
                     f"{bot_token}", Config.API_ID, Config.API_HASH,
                     bot_token=bot_token,
-                    plugins={"root": "bot/clone_plugins"},
+                    plugins={"root": "clone_plugins"},
                 )
                 await ai.start()
                 bot = await ai.get_me()
