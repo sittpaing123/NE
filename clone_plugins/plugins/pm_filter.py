fb1b920e55bb68f1d1104c5bfa74eae473bd321b
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2023-07-06 20:48:01 +0630

    Update pm_filter.py

--- clone_plugins/plugins/pm_filter.py
+++ clone_plugins/plugins/pm_filter.py
@@ -320,7 +320,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     if query.data.startswith("file"):
         ident, file_id = query.data.split("#")
-        files_ = await get_file_details(file_id)
+        files_ = await a_filter.get_file_details(file_id)
         user = query.message.reply_to_message.from_user.id
         if int(user) != 0 and query.from_user.id != int(user):
             return await query.answer(f"Hello {query.from_user.first_name},\nDon't Click Other Results!", show_alert=True)
