a723258e36e425b87fb1d860dc29ae187bd0daa5
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2023-07-06 22:16:39 +0630

    Update __init__.py

--- bot/client/__init__.py
+++ bot/client/__init__.py
@@ -45,4 +45,43 @@ class Client(PatchedClient):
         self.add_handler(handlers.MessageHandler(resolve_listener), group=-2)
         await super().start(*args, **kwargs)
 
-
+    async def iter_chat_history(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset_id: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
+        """Iterate through chat history sequentially.
+        This method iterates through the chat history in chronological order, starting from the specified offset_id.
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages), you can simply use "me" or "self".
+                For a contact that exists in your Telegram address book, you can use their phone number (str).
+            limit (``int``):
+                Maximum number of messages to be returned.
+            offset_id (``int``, *optional*):
+                Identifier of the message to start from.
+                Defaults to 0, which means the beginning of the chat history.
+        Returns:
+            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
+        Example:
+            .. code-block:: python
+                async for message in app.iter_chat_history("pyrogram", 100, offset_id=15000):
+                    print(message.text)
+        """
+        current = offset_id
+        while self.is_idling:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_chat_history(
+                chat_id,
+                limit=new_diff,
+                offset_id=current,
+                reverse=True
+            )
+            for message in messages:
+                yield message
+                current = message.message_id - 1
+            await asyncio.sleep(10)
