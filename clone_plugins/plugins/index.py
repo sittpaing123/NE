1f4305267bdea69f94960421f269863b463b2f8b
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2023-07-06 22:10:02 +0630

    Update index.py

--- clone_plugins/plugins/index.py
+++ clone_plugins/plugins/index.py
@@ -117,7 +117,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
         try:
             current = temp.CURRENT
             temp.CANCEL = False
-            async for message in bot.index_chat_history(chat, lst_msg_id, offset_id=temp.CURRENT):
+            async for message in bot.iter_chat_history(chat, lst_msg_id, offset_id=temp.CURRENT):
                 if temp.CANCEL:
                     await msg.edit(f"Successfully Cancelled!\n\nSaved <code>{total_files}</code> files to Database!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>\nUnsupported Media: <code>{unsupported}</code>\nErrors Occurred: <code>{errors}</code>")
                     break
